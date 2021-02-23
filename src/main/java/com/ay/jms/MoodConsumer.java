acb2f63473280690d21ef31c8dc2be2243049e35
Hamsin <sinceknee@foxmail.com>
2021-02-23 21:40:29 +0800

    clean

--- src/main/java/com/ay/jms/MoodConsumer.java
+++ src/main/java/com/ay/jms/MoodConsumer.java
@@ -5,7 +5,6 @@ import com.ay.request.MoodUnpraiseRequest;
 import com.ay.service.MoodService;
 import org.apache.activemq.command.ActiveMQObjectMessage;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 import javax.jms.JMSException;
